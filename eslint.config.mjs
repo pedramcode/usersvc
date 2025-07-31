64fd7714de48e551ab23b21d2543fc61837d8010
pedramcode <dev.dehghanpour@gmail.com>
2025-07-31 17:37:41 +0330

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,10 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
-    files: ["./src/**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: [
+      "./src/**/*.{js,mjs,cjs,ts,mts,cts}",
+      "./tests/**/*.{js,mjs,cjs,ts,mts,cts}",
+    ],
     rules: {
       'no-unused-vars': 'off',
       "@typescript-eslint/no-explicit-any": "off",
